c9f7d44a281c418bfe19abf33296b40b757ef577
Edwin Ko <edwin.mk.ko@gmail.com>
2024-02-01 03:35:43 +0000

    create schema and seeds for db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- users table seeds here
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Alice', 'A', 'alice@gmail.com', '123');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Bob', 'B', 'bob@gmail.com', '123');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Charlie', 'C', 'charlie@gmail.com', '123');
---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,6 @@
+-- categories table seeds here
+
+INSERT INTO categories (id, name) VALUES (1, 'Film / Series (To watch)');
+INSERT INTO categories (id, name) VALUES (2, 'Restaurants, cafes, etc. (To eat)');
+INSERT INTO categories (id, name) VALUES (3, 'Books (To read)');
+INSERT INTO categories (id, name) VALUES (4, 'Products (To buy)');
