da211d352c9c704ba3d6cf453721271b0a295ef2
Racingwind <louis.k.hu@gmail.com>
2024-02-03 12:11:39 -0800

    login page working

---
+++ db/seeds/03_tasks.sql
@@ -0,0 +1,9 @@
+-- tasks table seeds here
+INSERT INTO tasks (content, category_id, user_id, completed) VALUES ('Eat food', 2, 1, false);
+INSERT INTO tasks (content, category_id, user_id, completed) VALUES ('Watch a movie', 1, 1, true);
+
+INSERT INTO tasks (content, category_id, user_id, completed) VALUES ('Buy food', 4, 2, false);
+INSERT INTO tasks (content, category_id, user_id, completed) VALUES ('Read a book', 3, 2, false);
+
+INSERT INTO tasks (content, category_id, user_id, completed) VALUES ('Watch TV', 1, 3, true);
+INSERT INTO tasks (content, category_id, user_id, completed) VALUES ('Eat a cake', 2, 3, false);
