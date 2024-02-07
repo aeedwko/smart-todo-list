0eaba7603ed6a845637b5405ebaa624e3dc666cd
Edwin Ko <edwin.mk.ko@gmail.com>
2024-02-07 04:32:46 +0000

    comment out getCategory.js

--- getCategory.js
+++ getCategory.js
@@ -1,10 +1,39 @@
+// // require('dotenv').config();
+// // const OpenAI = require('openai');
+// // const openai = new OpenAI({
+// //     organization: 'org-8B0dcbPBpE9XugrzcTdKqIaw'
+// //   });
+
+// // const openaiApi = new OpenAI('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');
+
+// // const prompts = 'I need help with a task';
+// // const model = 'text-davinci-002';
+// // const examples = [
+// //   ['task1', 'category1'],
+// //   ['task2', 'category2'],
+// //   ['task3', 'category3'],
+// //   ['task4', 'category4']
+// // ];
+
+// // (async () => {
+// //   const gptResponse = openaiApi.classification({
+// //   model: model,
+// //   examples: examples,
+// //   query: prompts,
+// // }).then(response => {
+// //   console.log(response.data);
+// // }).catch(error => {
+// //   console.log(error);
+// // })
+// // });
+
 // require('dotenv').config();
-// const OpenAI = require('openai');
+// const OpenAI = require('./node_modules/openai');
 // const openai = new OpenAI({
-//     organization: 'org-8B0dcbPBpE9XugrzcTdKqIaw'
+//     organization: 'org-L9XUVzP0hOTCs07P9MB2927j'
 //   });
 
-// const openaiApi = new OpenAI('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');
+// const openaiApi = new OpenAI(process.env.OPENAI_API_KEY);
 
 // const prompts = 'I need help with a task';
 // const model = 'text-davinci-002';
@@ -27,42 +56,13 @@
 // })
 // });
 
-require('dotenv').config();
-const OpenAI = require('./node_modules/openai');
-const openai = new OpenAI({
-    organization: 'org-8B0dcbPBpE9XugrzcTdKqIaw'
-  });
+// async function main() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "system", content: "You are a helpful assistant." }],
+//     model: "gpt-3.5-turbo",
+//   });
 
-const openaiApi = new OpenAI(process.env.OPENAI_API_KEY);
+//   console.log(completion.choices[0]);
+// }
 
-const prompts = 'I need help with a task';
-const model = 'text-davinci-002';
-const examples = [
-  ['task1', 'category1'],
-  ['task2', 'category2'],
-  ['task3', 'category3'],
-  ['task4', 'category4']
-];
-
-(async () => {
-  const gptResponse = openaiApi.classification({
-  model: model,
-  examples: examples,
-  query: prompts,
-}).then(response => {
-  console.log(response.data);
-}).catch(error => {
-  console.log(error);
-})
-});
-
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
-
-  console.log(completion.choices[0]);
-}
-
-main();
+// main();
