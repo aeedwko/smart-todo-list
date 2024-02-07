ce2ba27b17ee7d290f3fef2812346c03fa530919
Edwin Ko <edwin.mk.ko@gmail.com>
2024-02-07 04:13:59 +0000

    remove .env

--- getCategory.js
+++ getCategory.js
@@ -1,5 +1,34 @@
-require('../node_modules/dotenv').config();
-const OpenAI = require('../node_modules/openai');
+// require('dotenv').config();
+// const OpenAI = require('openai');
+// const openai = new OpenAI({
+//     organization: 'org-8B0dcbPBpE9XugrzcTdKqIaw'
+//   });
+
+// const openaiApi = new OpenAI('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');
+
+// const prompts = 'I need help with a task';
+// const model = 'text-davinci-002';
+// const examples = [
+//   ['task1', 'category1'],
+//   ['task2', 'category2'],
+//   ['task3', 'category3'],
+//   ['task4', 'category4']
+// ];
+
+// (async () => {
+//   const gptResponse = openaiApi.classification({
+//   model: model,
+//   examples: examples,
+//   query: prompts,
+// }).then(response => {
+//   console.log(response.data);
+// }).catch(error => {
+//   console.log(error);
+// })
+// });
+
+require('dotenv').config();
+const OpenAI = require('./node_modules/openai');
 const openai = new OpenAI({
     organization: 'org-8B0dcbPBpE9XugrzcTdKqIaw'
   });
@@ -27,3 +56,13 @@ const examples = [
 })
 });
 
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
