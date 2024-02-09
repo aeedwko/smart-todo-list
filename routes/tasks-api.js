58d51b00729a2458f8ff7c896fcd56621fcde646
Edwin Ko <edwin.mk.ko@gmail.com>
2024-02-09 01:02:24 +0000

    integrate serpapi to choose category

--- routes/tasks-api.js
+++ routes/tasks-api.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const taskQueries = require('../db/queries/tasks');
+const apiCall = require('../getAllCategory');
 
 router.get('/', (req, res) => {
   taskQueries.getTasks()
@@ -17,18 +18,37 @@ router.get('/', (req, res) => {
 });
 
 router.post("/add", (req, res) => {
-  const task = {
-    user_id: req.body.user_id,
-    category_id: req.body.category_id,
-    content: req.body.content,
-    completed: req.body.completed
-  };
 
-  console.log(task);
+  const task_content = req.body.content;
 
-  taskQueries.addTask(task)
+  apiCall.getOrganicResultsForTask(task_content)
+    .then((results) => {
+      // Analyze keywords and categorize
+      let keywords = task_content.toLowerCase().split(" ");
+
+      results.forEach((result, index) => {
+        keywords += result.title + result.snippet;
+      });
+      keywords = keywords.toLowerCase().split(" ");
+      const category_id = apiCall.categorizeKeywords(keywords);
+
+      console.log(category_id);
+
+      // returns the appropriate category
+      return category_id;
+    })
+    .then(category_id => {
+      const task = {
+        user_id: req.body.user_id,
+        category_id: category_id,
+        content: req.body.content,
+        completed: req.body.completed
+      };
+
+      return taskQueries.addTask(task);
+    })
     .then(task => {
-      res.json({ task });
+      return res.json({ task });
     })
     .catch(err => {
       res
